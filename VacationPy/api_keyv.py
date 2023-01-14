9d6ecf18dc3b87c4fb66f61c7656f0caed3c7cf9
lordmeisiu <mcaelan@hotmail.com>
2023-01-14 15:18:28 -0500

    fished commit

---
+++ VacationPy/api_keyv.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = ""
+
+# Geoapify API Key
+geoapify_key = "748ddca46b524ca5b704bafce8e4648d"
---
+++ WeatherPy/api_keyw.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "00317220dff2092192f6c1bf10f7048b"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
